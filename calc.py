6b1cae3a25eed2064c764fd17263e31149ce4456
pedro <pedroluismontesinosrivera@gmail.com>
2019-03-21 00:54:38 -0400

    it saves the most recent calculation to a file

--- calc.py
+++ calc.py
@@ -6,25 +6,31 @@ print('Type \'quit\' to exit\n')
 previous = 0
 run = True
 
-def performMath():
-    global run #makes the run variable global
-    global previous
 
+def performMath():
+    global run  # makes the run variable global
+    global previous
+    equation = ""
     if previous == 0:
         equation = input('enter equation:')
-
     else:
         equation = input(str(previous))
+
     if equation == 'quit':
         run = False
     else:
         equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
         if previous == 0:
             previous = eval(equation)
+            fp = open("equations.txt", "w")
+            fp.write(str(previous))
+            fp.close()
         else:
-            previous == eval(str(previous) + equation)
+            previous = eval(str(previous) + equation)
+            fp = open("equations.txt", "w")
+            fp.write(str(previous)+)
+            fp.close()
+
 
 while run:
     performMath()
-
-    
