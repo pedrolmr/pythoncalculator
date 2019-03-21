5e7c5df57e45d827e409d1409d6c3e20ee0976a4
pedro <pedroluismontesinosrivera@gmail.com>
2019-03-21 00:03:05 -0400

    user is able to quit

--- calc.py
+++ calc.py
@@ -1,10 +1,21 @@
 import re
 
 print("calculator")
+print("Type 'quit' to exit\n")
 
 previous = 0
 run = True
 
 def performMath():
+    global run #makes the run variable global
+
     equation = input("enter equation:")
-    print(equation)
+    if equation == "quit":
+        run = False
+    else:
+        print("You typed", equation)
+
+while run:
+    performMath()
+
+    
